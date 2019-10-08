5145d4390cba4c1ca9424c0573275ead7f3a9396
1517043456 <1517043456@qq.com>
2019-10-08 14:58:25 +0800

    解决控制台传入和分析指令

---
+++ 201731054221/WordCount/WordCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "-i";
+            string s2 = "input";
+            string s3 = "-m";
+            string s4 = "3";
+            string[] s = {s1,s2,s3,s4 };
+            try
+            {
+                //将控制台传入的命令送去解析
+                SplitKeys splitKeys = new SplitKeys();
+                splitKeys.getKeys(args);
+                
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+           
+    }
+}
---
+++ 201731054221/WordCount/WordCount/SplitKeys.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    /**
+     * 用于解析指令
+     * */
+    class SplitKeys
+    {
+        public string getKeys(string[] keys) {
+            //指令的字符串
+            string stringKeys = null;
+            //装指令的list
+            Dictionary<int, string> keysDir = new Dictionary<int, string>();
+            //指令的计数，即如果第一个输入-i 则对应0 接着的input.txt为1
+            int count = 0;
+            int i = 0;
+            //解析命令
+            if (-1 == keys.Length) {
+                Console.WriteLine("指令不能为空！！！");
+            }
+            //将命令拼接起来
+            foreach (string str in keys) {
+                stringKeys += str;
+                keysDir.Add(count,str);
+                count++;
+            }
+            //判断含有那些指令
+            for (; i < keys.Length; i++) {
+                if (keys[i].Equals("-i")) {
+                    Console.WriteLine(keys[i+1]);
+                }
+                if (keys[i].Equals("-m"))
+                {
+                    Console.WriteLine(keys[i + 1]);
+                }
+                if (keys[i].Equals("-n"))
+                {
+                    Console.WriteLine(keys[i + 1]);
+                }
+                if (keys[i].Equals("-o"))
+                {
+                    Console.WriteLine(keys[i + 1]);
+                }
+            }
+            return null;
+        }
+    }
+}
